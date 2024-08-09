b7532419ff2365d5f9953b74af7d0d2164d33693
tanureet kaur <tanureetkaur@academin.rrc.ca>
2024-08-09 13:54:09 -0500

    updated

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -4,4 +4,13 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "should get index" do
+    get products_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get products_show_url
+    assert_response :success
+  end
 end
