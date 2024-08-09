bd1c9a1eec878a4ae6951e663df689c3612ba23b
tanureet kaur <tanureetkaur@academin.rrc.ca>
2024-08-09 18:49:26 -0500

    updated

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,9 @@ class Product < ApplicationRecord
   belongs_to :category
 
   has_one_attached :image
+
   mount_uploader :image, ImageUploader
+  
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :category_id, presence: true
