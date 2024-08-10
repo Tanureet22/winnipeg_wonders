cd52f65ab6f9a7ec71cc655b9e0471b260de6789
tanureet kaur <tanureetkaur@academin.rrc.ca>
2024-08-10 00:50:46 -0500

    added new method

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ApplicationRecord
   has_many :products
   validates :name, presence: true
+
+  def self.ransackable_associations(auth_object = nil)
+    %w[products]  # List the associations you want to make searchable
+  end
+  
 end
