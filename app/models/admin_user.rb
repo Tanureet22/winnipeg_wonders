a04d72db5afd5267aaa63ffd1ed40a0d23d229d5
tanureet kaur <tanureetkaur@academin.rrc.ca>
2024-08-10 00:49:11 -0500

    added

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,5 +1,11 @@
 class AdminUser < ApplicationRecord
-  
+
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
+
+  def self.ransackable_attributes(auth_object = nil)
+       %w[id email created_at current_sign_in_at last_sign_in_at sign_in_count updated_at]
+     end
+     
 end
--- config/application.rb
+++ config/application.rb
@@ -10,5 +10,6 @@ module WinnipegWonders
   class Application < Rails::Application
     config.load_defaults 7.1
     config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_paths += %W(#{config.root}/app/controllers/concerns)
   end
 end
